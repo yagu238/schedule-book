681e5596b51de3c12a0c1b59a6c65b8349d6a216
yagu238 <yagu238.dev@gmail.com>
2021-09-06 00:48:54 +0900

    tailwindcss test

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    enabled: process.env.TAILWIND_MODE === 'build',
+    content: [
+      './src/**/*.{html,scss,ts}'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
